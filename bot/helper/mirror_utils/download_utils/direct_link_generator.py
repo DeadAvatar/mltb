b765c458f2edac13ef848c5d1abe7298b7594540
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2022-11-04 16:47:37 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -506,7 +506,7 @@ def udrive(url: str) -> str:
     
     if "hubdrive" in url:
         if "hubdrive.in" in url:
-            url = url.replace(".in",".pro",".cc")
+            url = url.replace(".in",".pro",".cc",".pw")
         client.cookies.update({'crypt': HUBDRIVE_CRYPT})
     if 'drivehub' in url:
         client.cookies.update({'crypt': KATDRIVE_CRYPT})
